3309ca6155aec179bed90962d7cb0b6ecf68da0f
karthik2804 <karthik@karthik.localdomain>
2020-05-06 16:36:56 +0530

    added auto_resize option

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ class LineChart {
             width = 500,
             height = 300,
             xaxis_offset = 280,
-            yaxis_offset = 30
+            yaxis_offset = 30,
+            auto_resize = 1
         this.height = function(value) {
             if (!arguments.length) return height
                 height = value
@@ -34,10 +35,20 @@ class LineChart {
             if (!arguments.length) return yaxis_offset
             yaxis_offset = value
         }
-        this.el = svg("svg", {width:0, height:0})
+        this.auto_resize = function(value) {
+            if (!arguments.length) return auto_resize
+            auto_resize = value
+        }
+        this.el = svg("svg", {id:"graph", width:0, height:0})
             
     }
     update() {
+        if(this.auto_resize() > 0) {
+        let width = this.el.parentElement.clientWidth
+        this.width(width/2)
+        this.height(width * 0.28)
+        this.xaxis_offset(width * 0.28 -20)
+        }
         var x = d3.scaleLinear().domain([0, d3.max(data, d => d.x)]).range([this.margin().left, this.width() - this.margin().right])
         var y = d3.scaleLinear().domain([0, d3.max(data, d => d.y)]).range([this.height() - this.margin().bottom, this.margin().top])
         var x_axis = d3.axisBottom().scale(x)
@@ -55,10 +66,16 @@ class LineChart {
 
 
 let graph = new LineChart()
-let total = el("div", graph, el("div.pa2", el("br"), el("h3", "docs"), el("p", "Use graph.<property>() to get the height of the graph and graph.<property>(x) to set height to x."),
-    el("p", "Properties available are height,width, xaxis_offset, yaxis_offset. margin is a read-only property."),
+let total = el("div.w-100", graph, el("div.pa2", el("br"), el("h3", "docs"), el("p", "Use graph.<property>() to get the height of the graph and graph.<property>(x) to set height to x."),
+    el("p", "Properties available are height,width, xaxis_offset, yaxis_offset, auto_resize. margin is a read-only property."),
     el("p", "Use graph.update() to update graph"),
-    el("p", "To change values of plotted point edit variable data")))
+    el("p", "To change values of plotted point edit variable data"),
+    el("p", "Set auto_resize > 0 to enable and auto_resize = 0 to disable.(default = enabled)")))
 
 mount(document.body, total);
 graph.update()
+
+
+window.onresize = function() {
+    graph.update()
+}
